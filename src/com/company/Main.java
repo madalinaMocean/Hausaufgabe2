a126072040a8f6bf38c76dc20a22b1b6a4f52b43
madalinaMocean <maria.madalina.mocean@stud.ubbcluj.ro>
2021-11-04 14:03:05 +0200

    Java conventions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-         Basketball b1=new Basketball();
-         Basketball b2=new Basketball();
-         Fussball f1=new Fussball();
-         Fussball f2=new Fussball();
-         Hindernislauf hi1 = new Hindernislauf();
-         Hindernislauf hi2=new Hindernislauf();
-         Hochsprung ho1=new Hochsprung();
-         Hochsprung ho2=new Hochsprung();
+         Basketball basketball1=new Basketball();
+         Basketball basketball2=new Basketball();
+         Fussball fussball1=new Fussball();
+         Fussball fussball2=new Fussball();
+         Hindernislauf hindernislauf1 = new Hindernislauf();
+         Hindernislauf hindernislauf2=new Hindernislauf();
+         Hochsprung hochsprung1=new Hochsprung();
+         Hochsprung hochsprung2=new Hochsprung();
 
-         List<Sport> sportUser1 = List.of(new Sport[]{b1, f2, hi1});
+         List<Sport> sportUser1 = List.of(new Sport[]{basketball1, fussball2, hindernislauf1});
          Benutzer user1=new Benutzer("Pop","Andrei",sportUser1);
          System.out.println();
          System.out.println("Added new user with name: " + user1.getVorName()+" "+ user1.getNachName()+ ", and Sport Types:"+
@@ -23,8 +23,8 @@ public class Main {
 
          System.out.print("Added times of Sport Durations from user " + user1.getVorName()+" "+ user1.getNachName()+ ": ");
          System.out.println(user1.kalkuliereZeit());
-         System.out.print("Added times of Sport Durations from user, for sport of given type("+ b1.getClass().toString().substring(18) +"): ");
-         System.out.println(user1.kalkuliereZeit(b1));
+         System.out.print("Added times of Sport Durations from user, for sport of given type("+ basketball1.getClass().toString().substring(18) +"): ");
+         System.out.println(user1.kalkuliereZeit(basketball1));
          System.out.print("Average times of Sport Durations from user, " + user1.getVorName()+" "+ user1.getNachName()+": ");
          System.out.println(user1.kalkuliereDurschnittszeit());
     }
